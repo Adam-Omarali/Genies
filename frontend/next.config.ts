c1bf5b06deb387a65ac1b90100a9f45850295e48
Adam <adamomarali37@gmail.com>
2025-03-21 09:05:34 -0400

    fixed image config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,20 +5,26 @@ console.log(process.env.NEXT_PUBLIC_BACKEND_URL?.split("//")[1].split(":")[0]);
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
+      // {
+      //   protocol: 'http',
+      //   hostname: process.env.NEXT_PUBLIC_BACKEND_URL?.split("//")[1].split(":")[0].split("/")[0] ?? 'localhost',
+      //   port: process.env.NEXT_PUBLIC_BACKEND_PORT,
+      //   pathname: '/static/**',
+      //   search: '',
+      // },
       {
-        protocol: 'http',
-        hostname: process.env.NEXT_PUBLIC_BACKEND_URL?.split("//")[1].split(":")[0] ?? 'localhost',
-        port: process.env.NEXT_PUBLIC_BACKEND_PORT,
-        pathname: '/static/**',
-        search: '',
+        protocol: 'https',
+        hostname: 'genies-9ibi.onrender.com',
+        port: '',
+        pathname: '/api/static/**',
       },
-      {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '8000',
-        pathname: '/static/**',
-        search: '',
-      },
+      // {
+      //   protocol: 'http',
+      //   hostname: '127.0.0.1',
+      //   port: '8000',
+      //   pathname: '/static/**',
+      //   search: '',
+      // },
     ],
   }  /* config options here */
 };
